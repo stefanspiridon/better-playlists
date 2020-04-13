b514e6fc7164c9bac38cc4f48c972c14542d532f
Stefan <bd.stefanspiridon@gmail.com>
2020-04-13 19:32:06 +0100

    added more fake server data

--- src/App.js
+++ src/App.js
@@ -1,62 +1,54 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = "white";
-
 let defaultStyle = {
-  color: defaultTextColor
-
+  color: '#fff'
 };
-
 let fakeServerData = {
   user: {
-    name : 'David',
+    name: 'David',
     playlists: [
-    {
-      name: 'May favs',
-      songs: 
-      [
-        {name: 'aaaaaa', duration: 1111}, 
-        {name: 'bbbbbb', duration: 2222},
-        {name :'cccccc', duration: 3333}
-      ]
-    },
-    {
-      name: 'May favs2',
-      songs: 
-      [
-        {name: 'aaaaaa', duration: 1111}, 
-        {name: 'bbbbbb', duration: 2222},
-        {name :'cccccc', duration: 3333}
-      ]
-    },
-    {
-      name: 'May favs3',
-      songs: 
-      [
-        {name: 'aaaaaa', duration: 1111}, 
-        {name: 'bbbbbb', duration: 2222},
-        {name :'cccccc', duration: 3333}
-      ]
-    },
-    {
-      name: 'May favs3',
-      songs: 
-      [
-        {name: 'aaaaaa', duration: 1111}, 
-        {name: 'bbbbbb', duration: 2222},
-        {name :'cccccc', duration: 3333}
-      ]
-    }
-  ]
+      {
+        name: 'My favorites',
+        songs: [
+          {name: 'Beat It', duration: 1345}, 
+          {name: 'Cannelloni Makaroni', duration: 1236},
+          {name: 'Rosa helikopter', duration: 70000}
+        ]
+      },
+      {
+        name: 'Discover Weekly',
+        songs: [
+          {name: 'Beat It', duration: 1345}, 
+          {name: 'Cannelloni Makaroni', duration: 1236},
+          {name: 'Rosa helikopter', duration: 70000}
+        ]
+      },
+      {
+        name: 'Another playlist - the best!',
+        songs: [
+          {name: 'Beat It', duration: 1345}, 
+          {name: 'Hallelujah', duration: 1236},
+          {name: 'Rosa helikopter', duration: 70000}
+        ]
+      },
+      {
+        name: 'Playlist - yeah!',
+        songs: [
+          {name: 'Beat It', duration: 1345}, 
+          {name: 'Cannelloni Makaroni', duration: 1236},
+          {name: 'Hej Hej Monika', duration: 70000}
+        ]
+      }
+    ]
   }
-}
+};
 
 class PlaylistCounter extends Component {
-  render () {
+  render() {
     return (
-      <div style={{width: "40%", display: "inline-block"}}>
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
         <h2>{this.props.playlists.length} playlists</h2>
       </div>
     );
@@ -64,15 +56,15 @@ class PlaylistCounter extends Component {
 }
 
 class HoursCounter extends Component {
-  render () {
+  render() {
     let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
       return songs.concat(eachPlaylist.songs)
     }, [])
     let totalDuration = allSongs.reduce((sum, eachSong) => {
       return sum + eachSong.duration
-    }, 0) 
+    }, 0)
     return (
-      <div style={{width: "40%", display: "inline-block"}}>
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
         <h2>{Math.round(totalDuration/60)} hours</h2>
       </div>
     );
@@ -82,25 +74,25 @@ class HoursCounter extends Component {
 class Filter extends Component {
   render() {
     return (
-      <div>
+      <div style={defaultStyle}>
         <img/>
         <input type="text"/>
-        Filter
       </div>
     );
   }
 }
 
 class Playlist extends Component {
-  render () {
+  render() {
+    let playlist = this.props.playlist
     return (
-      <div style = {{...defaultStyle, width: "25%", display: "inline-block"}}>
-        <img/>
-        <h3>Playlist Name</h3>
+      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
+        <img />
+        <h3>{playlist.name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {playlist.songs.map(song => 
+            <li>{song.name}</li>
+          )}
         </ul>
       </div>
     );
@@ -109,33 +101,30 @@ class Playlist extends Component {
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {serverData: {}}
   }
-  componentDidMount(){
+  componentDidMount() {
     setTimeout(() => {
       this.setState({serverData: fakeServerData});
     }, 1000);
   }
-  render () {
+  render() {
+
     return (
-      <div className='App'>
+      <div className="App">
         {this.state.serverData.user ?
         <div>
-          <h1>
-            {this.state.serverData.user &&
-              this.state.serverData.user.name}'s playlists
+          <h1 style={{...defaultStyle, 'font-size': '54px'}}>
+            {this.state.serverData.user.name}'s Playlists
           </h1>
-          <PlaylistCounter playlists={this.state.serverData.user &&
-            this.state.serverData.user.playlists}/>
-          <HoursCounter playlists={this.state.serverData.user &&
-            this.state.serverData.user.playlists}/>  
+          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+          <HoursCounter playlists={this.state.serverData.user.playlists}/>
           <Filter/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-        </div> : <h1>Loading...</h1>
+          {this.state.serverData.user.playlists.map(playlist => 
+            <Playlist playlist={playlist} />
+          )}
+        </div> : <h1 style={defaultStyle}>Loading...</h1>
         }
       </div>
     );
@@ -143,3 +132,5 @@ class App extends Component {
 }
 
 export default App;
+
+
