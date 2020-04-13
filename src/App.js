3514753003df97edfec7641055365f72de79dba5
Stefan <bd.stefanspiridon@gmail.com>
2020-04-13 16:46:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let defaultTextColor = "white";
+
+let defaultStyle = {
+  color: defaultTextColor
+
+};
+
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{width: "40%", display: "inline-block"}}>
+        <h2>Number text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <img/>
+        <input type="text"/>
+        Filter
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render () {
+    return (
+      <div style = {{...defaultStyle, width: "25%", display: "inline-block"}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
 }
 
 export default App;
